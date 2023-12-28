80477937f116beb1da7c20ae82862c00000ee78e
mazin abed <mazinabed@mazins-MacBook-Pro.local>
2023-12-28 12:23:58 +0300

    adding code

--- bot.js
+++ bot.js
@@ -217,7 +217,9 @@
 
 
 const { Telegraf } = require("telegraf");
-const TOKEN = "6456398897:AAEP2j09JLgqluudftxWMSxDujmFT4sws6g";
+require('dotenv').config();
+const TOKEN = process.env.TOKEN
+//"6456398897:AAEP2j09JLgqluudftxWMSxDujmFT4sws6g";
 const bot = new Telegraf(TOKEN);
 
 const web_link = "https://testingbot-8rcz.onrender.com";
