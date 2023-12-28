bc9880641cb3465c803e65a1faa57391c530385d
mazin abed <mazinabed@mazins-MacBook-Pro.local>
2023-12-28 12:18:22 +0300

    adding code

--- bot.js
+++ bot.js
@@ -1,85 +1,102 @@
-const { Telegraf } = require("telegraf");
-const TOKEN = "6456398897:AAEP2j09JLgqluudftxWMSxDujmFT4sws6g";
-const bot = new Telegraf(TOKEN);
+// const { Telegraf } = require("telegraf");
+// const TOKEN = "6456398897:AAEP2j09JLgqluudftxWMSxDujmFT4sws6g";
+// const bot = new Telegraf(TOKEN);
 
-const web_link = "https://testingbot-8rcz.onrender.com";
-const channelId = "@babyloncenter_net"; // Replace with your channel username or ID
-bot.start((ctx) =>
-  ctx.reply("Welcome :)))))", {
-    reply_markup: {
-      keyboard: [[{ text: "web app", web_app: { url: web_link } }]],
-    },
-  })
-);
-bot.on('message', (ctx) => {
-  // try {
-  //   const receivedData = JSON.parse(ctx.message.web_app_data.data);
-  //   console.log('Received data from user:', receivedData);
-  //   const Data2 = receivedData.cartItems[0].tilte
-  //   return ctx.reply(Data2)
-    // Process the received data as needed
-    try {
-      const receivedData = JSON.parse(ctx.message.web_app_data.data);
+// const web_link = "https://testingbot-8rcz.onrender.com";
+// const channelId = "@babyloncenter_net"; // Replace with your channel username or ID
+// bot.start((ctx) =>
+//   ctx.reply("Welcome :)))))", {
+//     reply_markup: {
+//       keyboard: [[{ text: "web app", web_app: { url: web_link } }]],
+//     },
+//   })
+// );
+// bot.on('message', (ctx) => {
+//   // try {
+//   //   const receivedData = JSON.parse(ctx.message.web_app_data.data);
+//   //   console.log('Received data from user:', receivedData);
+//   //   const Data2 = receivedData.cartItems[0].tilte
+//   //   return ctx.reply(Data2)
+//     // Process the received data as needed
+//     try {
+//       const receivedData = JSON.parse(ctx.message.web_app_data.data);
   
-      // Extract cartItems from received data
-      const cartItems = receivedData.cartItems;
-      const phoneNumber = receivedData.phoneNumber;
-      const address = receivedData.address;
-      // Assuming cartItems is an array
-      for (const item of cartItems) {
-        const title = item.title;
-        const price = item.price;
+//       // Extract cartItems from received data
+//       const cartItems = receivedData.cartItems;
+//       const phoneNumber = receivedData.phoneNumber;
+//       const address = receivedData.address;
+//       // Assuming cartItems is an array
+//       for (const item of cartItems) {
+//         const title = item.title;
+//         const price = item.price;
   
-        // Now you can use title and price as needed
-        console.log('Received item - Title:', title, 'Price:', price);
-  console.log(receivedData)
-   // Send the order details to the Telegram channel
+//         // Now you can use title and price as needed
+//         console.log('Received item - Title:', title, 'Price:', price);
+//   console.log(receivedData)
+//    // Send the order details to the Telegram channel
  
 
-        // Send a response to the user or perform any other actions
-        ctx.reply(
-         ` ${ctx.from.first_name}\n`+
-          ` Received item:\n` +
-          `- *Title*: ${title}\n` +
-          `- *Price*: ${price}\n` +
-          `- *Phone Number*: ${phoneNumber}\n` +
-          `- *Address*: ${address}`
-        );
-        const message = `New order received:\n` +
-        ` ${ctx.from.first_name}\n`+
-        `- *Title*: ${title}\n` +
-        `- *Price*: ${price}\n` +
-        `- *Phone Number*: ${phoneNumber}\n` +
-        `- *Address*: ${address}`;
+//         // Send a response to the user or perform any other actions
+//         ctx.reply(
+//          ` ${ctx.from.first_name}\n`+
+//           ` Received item:\n` +
+//           `- *Title*: ${title}\n` +
+//           `- *Price*: ${price}\n` +
+//           `- *Phone Number*: ${phoneNumber}\n` +
+//           `- *Address*: ${address}`
+//         );
+//         const message = `New order received:\n` +
+//         ` ${ctx.from.first_name}\n`+
+//         `- *Title*: ${title}\n` +
+//         `- *Price*: ${price}\n` +
+//         `- *Phone Number*: ${phoneNumber}\n` +
+//         `- *Address*: ${address}`;
      
-      bot.telegram.sendMessage(channelId, message, { parse_mode: 'Markdown' });
-        //ctx.reply(`Received additional info - Phone Number: ${phoneNumber}, Address: ${address}`);
-        bot.on('message', (ctx) => {
-          const chatId = ctx.chat.id;
-          console.log('Chat ID:', chatId);
-        });
-      }
+//       bot.telegram.sendMessage(channelId, message, { parse_mode: 'Markdown' });
+//         //ctx.reply(`Received additional info - Phone Number: ${phoneNumber}, Address: ${address}`);
+//         bot.on('message', (ctx) => {
+//           const chatId = ctx.chat.id;
+//           console.log('Chat ID:', chatId);
+//         });
+//       }
 
-  } catch (error) {
-    console.error('Error parsing received data:', error);
-  }
-});
-// bot.on("message", async (ctx) => {
-//   console.log(ctx.message.web_app_data)
-//   return ctx.reply(ctx.message.web_app_data.data)
-
+//   } catch (error) {
+//     console.error('Error parsing received data:', error);
+//   }
 // });
-bot.on('text', (ctx) => {
-  const Data = ctx.message.text;
-  console.log('Received data from user:', Data);
-  // Process the received data as needed
-});
+// // bot.on("message", async (ctx) => {
+// //   console.log(ctx.message.web_app_data)
+// //   return ctx.reply(ctx.message.web_app_data.data)
+
+// // });
+// bot.on('text', (ctx) => {
+//   const Data = ctx.message.text;
+//   console.log('Received data from user:', Data);
+//   // Process the received data as needed
+// });
 
 
 
 
 
-bot.launch();
+// // bot.launch();
+// bot.launch({
+//   webhook: {
+//     // Public domain for webhook; e.g.: example.com
+//     domain: "https://testingbot-8rcz.onrender.com/api",
+
+//     // Port to listen on; e.g.: 8080
+//     port: 3000,
+
+//     // Optional path to listen for.
+//     // `bot.secretPathComponent()` will be used by default
+//    // path: webhookPath,
+
+//     // Optional secret to be sent back in a header for security.
+//     // e.g.: `crypto.randomBytes(64).toString("hex")`
+//     //secretToken: randomAlphaNumericString,
+//   },
+// });
 
 // const { Telegraf } = require("telegraf");
 // const express = require("express");
@@ -197,3 +214,87 @@ bot.launch();
 
 // // Start the bot
 // bot.launch();
+
+
+const { Telegraf } = require("telegraf");
+const TOKEN = "6456398897:AAEP2j09JLgqluudftxWMSxDujmFT4sws6g";
+const bot = new Telegraf(TOKEN);
+
+const web_link = "https://testingbot-8rcz.onrender.com";
+const channelId = "@babyloncenter_net"; // Replace with your channel username or ID
+bot.start((ctx) =>
+  ctx.reply("Welcome :)))))", {
+    reply_markup: {
+      keyboard: [[{ text: "web app", web_app: { url: web_link } }]],
+    },
+  })
+);
+bot.on('message', (ctx) => {
+  // try {
+  //   const receivedData = JSON.parse(ctx.message.web_app_data.data);
+  //   console.log('Received data from user:', receivedData);
+  //   const Data2 = receivedData.cartItems[0].tilte
+  //   return ctx.reply(Data2)
+    // Process the received data as needed
+    try {
+      const receivedData = JSON.parse(ctx.message.web_app_data.data);
+  
+      // Extract cartItems from received data
+      const cartItems = receivedData.cartItems;
+      const phoneNumber = receivedData.phoneNumber;
+      const address = receivedData.address;
+      // Assuming cartItems is an array
+      for (const item of cartItems) {
+        const title = item.title;
+        const price = item.price;
+  
+        // Now you can use title and price as needed
+        console.log('Received item - Title:', title, 'Price:', price);
+  console.log(receivedData)
+   // Send the order details to the Telegram channel
+ 
+
+        // Send a response to the user or perform any other actions
+        ctx.reply(
+         ` ${ctx.from.first_name}\n`+
+          ` Received item:\n` +
+          `- *Title*: ${title}\n` +
+          `- *Price*: ${price}\n` +
+          `- *Phone Number*: ${phoneNumber}\n` +
+          `- *Address*: ${address}`
+        );
+        const message = `New order received:\n` +
+        ` ${ctx.from.first_name}\n`+
+        `- *Title*: ${title}\n` +
+        `- *Price*: ${price}\n` +
+        `- *Phone Number*: ${phoneNumber}\n` +
+        `- *Address*: ${address}`;
+     
+      bot.telegram.sendMessage(channelId, message, { parse_mode: 'Markdown' });
+        //ctx.reply(`Received additional info - Phone Number: ${phoneNumber}, Address: ${address}`);
+        bot.on('message', (ctx) => {
+          const chatId = ctx.chat.id;
+          console.log('Chat ID:', chatId);
+        });
+      }
+
+  } catch (error) {
+    console.error('Error parsing received data:', error);
+  }
+});
+// bot.on("message", async (ctx) => {
+//   console.log(ctx.message.web_app_data)
+//   return ctx.reply(ctx.message.web_app_data.data)
+
+// });
+bot.on('text', (ctx) => {
+  const Data = ctx.message.text;
+  console.log('Received data from user:', Data);
+  // Process the received data as needed
+});
+
+
+
+
+
+bot.launch();
